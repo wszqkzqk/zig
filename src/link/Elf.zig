23bbbf2fadfc5894f962f87202feff3d22c68752
Jakub Konka <kubkon@jakubkonka.com>
2024-10-02 13:32:13 +0200

    elf: clear dynamic relocs before resolving relocs in atoms
    When resolving and writing atoms to file, we may add dynamic relocs
    to the output buffer so clear the buffers before that happens.

--- src/link/Elf.zig
+++ src/link/Elf.zig
@@ -1032,6 +1032,9 @@ pub fn flushModule(self: *Elf, arena: Allocator, tid: Zcu.PerThread.Id, prog_nod
     for (self.objects.items) |index| {
         self.file(index).?.object.dirty = false;
     }
+    // TODO: would state tracking be more appropriate here? perhaps even custom relocation type?
+    self.rela_dyn.clearRetainingCapacity();
+    self.rela_plt.clearRetainingCapacity();
 
     if (self.zigObjectPtr()) |zo| {
         var has_reloc_errors = false;
@@ -1062,6 +1065,7 @@ pub fn flushModule(self: *Elf, arena: Allocator, tid: Zcu.PerThread.Id, prog_nod
     try self.writeShdrTable();
     try self.writeAtoms();
     try self.writeMergeSections();
+
     self.writeSyntheticSections() catch |err| switch (err) {
         error.RelocFailure => return error.FlushFailure,
         error.UnsupportedCpuArch => {
@@ -4403,8 +4407,6 @@ fn writeSyntheticSections(self: *Elf) !void {
     }
 
     if (self.rela_dyn_section_index) |shndx| {
-        // TODO: would state tracking be more appropriate here? perhaps even custom relocation type?
-        self.rela_dyn.clearRetainingCapacity();
         const shdr = slice.items(.shdr)[shndx];
         try self.got.addRela(self);
         try self.copy_rel.addRela(self);
@@ -4437,8 +4439,6 @@ fn writeSyntheticSections(self: *Elf) !void {
     }
 
     if (self.rela_plt_section_index) |shndx| {
-        // TODO: would state tracking be more appropriate here? perhaps even custom relocation type?
-        self.rela_plt.clearRetainingCapacity();
         const shdr = slice.items(.shdr)[shndx];
         try self.plt.addRela(self);
         try self.base.file.?.pwriteAll(mem.sliceAsBytes(self.rela_plt.items), shdr.sh_offset);
